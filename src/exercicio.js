7079d247607e08d70b2d5552f3e51c9103205faa
hibernon rodrigues <hibernon73@gmail.com>
2024-07-04 21:02:05 -0300

    primeiro commit

---
+++ src/exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+const numeros = (num, num2) => num * num2;
+const resultado = numeros(38, 42);
+console.log(resultado);
+const nome = (saudacao, nome) => saudacao + ' ' + nome;
+const chamar = nome('ola', 'João');
+console.log(chamar);
---
+++ src/exercicio.ts
@@ -0,0 +1,14 @@
+ const numeros = (num:number, num2:number): number => num * num2
+
+const resultado = numeros(38,42);
+console.log(resultado)
+
+const nome = (saudacao:string,nome:string): string => saudacao + ' ' + nome
+
+const chamar = nome('ola', 'João')
+console.log(chamar)
+
+
+
+
+
